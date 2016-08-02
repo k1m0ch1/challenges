43fc1db6aaf07c969ca7e8dc3991e60803826aac
k1m0ch1 <yahya.kimochi@gmail.com>
2016-08-02 15:47:45 +0700

    fizzbuzz 1.2 add ternary operator

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,12 +1,9 @@
 def main():
 	for x in range(1, 21):
 		hasil = ""
-		if x%3==0:
-			hasil += "fizz"
-		if x%5==0:
-			hasil += "buzz"
-		if hasil == "":
-			hasil = x
+		hasil += "fizz" if x%3==0 else ""
+		hasil += "buzz" if x%5==0 else ""
+		hasil = x if hasil=="" else hasil
 
 		print hasil
 	return;
