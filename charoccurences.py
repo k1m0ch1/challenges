918bb63a9e5c95e18dc46c18c2dccbc6abd083b7
k1m0ch1 <yahya.kimochi@gmail.com>
2016-08-02 18:22:41 +0700

    char occurences 0.3

--- charoccurences.py
+++ charoccurences.py
@@ -1,11 +1,11 @@
 def main():
-	print occurences("What The Fuck was happen?!?!?!..")
+	print occurences("!@#$!!@ What The Fuck was happen ?!?!?!..")
 	return;
 
 #this only count small letter
 #unicode characte un-supported
 def occurences(word):
-	alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+	alphabet = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ "
 	hasil = ""
 	for x in alphabet:
 		hasil += str(x) + "," + str(word.count(x)) + "\n" if word.count(x)>0 else ""
