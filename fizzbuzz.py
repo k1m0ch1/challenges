88726832ab776d42b49f7b7c1d62c0d30d4661e6
k1m0ch1 <yahya.kimochi@gmail.com>
2016-08-02 15:37:41 +0700

    fizzbuzz 1.0

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,16 @@
 def main():
-	for x in range(1, 16):
+	for x in range(1, 21):
 		print fizzorbuzz(x)
 	return;
 
 def fizzorbuzz(angka):
-	hasil = angka;
+	hasil = "";
 	if angka%3==0:
-		hasil = "fizz";
-	elif angka%5==0:
-		hasil = "buzz";
+		hasil += "fizz"
+	if angka%5==0:
+		hasil += "buzz"
+	if hasil=="":
+		hasil = angka
 
 	return hasil;
 
